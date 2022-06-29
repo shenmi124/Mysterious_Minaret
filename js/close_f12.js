0c042c2823570966fc97319321a0826a0cc4e728
shenmi124 <67265011@qq.com>
2022-06-29 14:36:41 +0800

    Update close_f12.js

--- js/close_f12.js
+++ js/close_f12.js
@@ -14,20 +14,3 @@ if(document.all){
 		return true
 	}
 }
-
-document.oncopy = function(event){
-	if(window.event){
-		event = window.event
-	}
-	try{
-		var the = event.srcElement
-		if(
-			!((the.tagName == 'INPUT' && the.type.toLowerCase() == 'text') ||  the.tagName == 'TEXTAREA')
-		){
-			return false
-		}
-		return true
-	}catch(e){
-		return false
-	}
-}
